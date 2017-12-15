b845a5b72c236a73a1d98ed1a11f136977209b9f
Wessel Pieterse <wessel@ordercloud.co.za>
2017-12-15 14:37:39 +0200

    MAde redux work
    Added login, receiveUser and logout events

--- redux/redux-action-types.js
+++
@@ -1,7 +0,0 @@
-
-        //users
-        export const INVALIDATE_USER = 'INVALIDATE_USER';
-        export const REQUEST_USER=  'REQUEST_USER';
-        export const RECEIVE_USER = 'RECEIVE_USER';
-        export const LOGIN_USER = 'LOGIN_USER';
-        export const LOGOUT_USER = 'LOGOUT_USER';
